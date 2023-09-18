4f186f1119a0b2c7217811899a673e93c4b343df
Daniella :3 <112992169+Daniellachu@users.noreply.github.com>
2023-09-18 14:04:40 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hallow CodeSandbox</h1>
+      <h2>this is a test github page</h2>
+    </div>
+  );
+}
